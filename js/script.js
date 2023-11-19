c8d35d53a9692e5a40404cc6d18b7ea3d5729609
JkevinJ <kevin.jasahri@gmail.com>
2023-11-19 23:25:06 +0100

    initiation portfolio

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(() => {
+  let typed = new Typed('.text-intro', {
+      strings: ['dévelloppeur web', 'étudiant en BTS SIO option SLAM'],
+      typeSpeed: 80,
+      backSpeed: 60,
+      loop: true
+  });
+});
