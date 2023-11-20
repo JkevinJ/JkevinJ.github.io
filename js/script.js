bae45f3598a8fe78cab74e339bd37888ad9ea988
JkevinJ <kevin.jasahri@gmail.com>
2023-11-20 16:54:23 +0100

    mj3

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(() => {
   let typed = new Typed('.text-intro', {
-      strings: ['dévelloppeur web', 'étudiant en BTS SIO option SLAM'],
+      strings: ['développeur web', 'étudiant en BTS SIO option SLAM'],
       typeSpeed: 80,
       backSpeed: 60,
       loop: true
