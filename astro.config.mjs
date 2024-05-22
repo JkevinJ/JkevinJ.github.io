6990e9e395b3b7e748d81449e9d0ea0354a9d9f9
JkevinJ <kevin.jasahri@gmail.com>
2024-05-22 09:38:37 +0200

    test6

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   outDir: 'dist',
-  base: '/public',
+  base: 'https://jkevinj.github.io/public/',
   site: 'https://JkevinJ.github.io',
 });
